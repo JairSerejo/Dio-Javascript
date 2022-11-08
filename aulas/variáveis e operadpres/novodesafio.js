ec33dccaea398e946248898e8973bd85831b6b4a
JairSerejo <j.serejoqi@gmail.com>
2022-11-08 20:57:29 -0300

    treinamento

--- aulas/variáveis e operadpres/novodesafio.js
+++ aulas/variáveis e operadpres/novodesafio.js
@@ -17,4 +17,10 @@ const nota3 = 6;
 const media = (nota1 + nota2 + nota3) / 3
 console.log (media);
 
-    
+    if (media <5) {
+        console.log ('reprovado');
+    } else if (media >=5 && media <=7) {
+        console.log ('recuperação');
+    } else {
+        console.log ('aprovado');
+    }
