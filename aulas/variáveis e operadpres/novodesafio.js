1390cf4256c5d4375a9ff0778df3a78ac460e870
JairSerejo <j.serejoqi@gmail.com>
2022-11-08 20:34:24 -0300

    nova aula

---
+++ aulas/variáveis e operadpres/novodesafio.js
@@ -0,0 +1,20 @@
+/*
+Faça um algoritimo que dado as as 3 notas tiradas por um aluno em um
+semestre da faculdade calcule e imprima a sua media e a sua classificação
+conforme a tabela abaixo.
+
+Média = (nota 1 + nota 2 + nota 3) / 3;
+
+Classificação:
+- Media menor que 5, reprovado;
+- Media entre 5 e 7, recuperação;
+- Media acima de 7, aprovado;
+*/
+
+const nota1 = 5;
+const nota2 = 7;
+const nota3 = 6;
+const media = (nota1 + nota2 + nota3) / 3
+console.log (media);
+
+    
