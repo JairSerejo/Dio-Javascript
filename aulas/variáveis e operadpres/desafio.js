2db8439d718dd8848840b5609a8637d0a6e55682
JairSerejo <j.serejoqi@gmail.com>
2022-11-08 19:47:02 -0300

    Implementação de novas variaveis

--- aulas/variáveis e operadpres/desafio.js
+++ aulas/variáveis e operadpres/desafio.js
@@ -1,16 +1,26 @@
 /* Faça um programa para caucular o gasto com um consumo de combustivel 
 em uma viagem.
 
-Você tera 3 variaveis. sendo elas:
-1 - preço do combustivel;
-2 - gasto medio de combustivel do carro por km;
-3  - distancia em km da viagem;
+Você tera 5 variaveis. sendo elas:
+1 - preço do Etanol;
+2 - preço da Gasolina;
+3 - tipo de combustivel que está no seu carro;
+4 - gasto medio de combustivel do carro por km;
+5  - distancia em km da viagem;
 
 */
-const precoCombustivel = 5.79;
+const precoEtanol = 5.79;
+const precogasolina = 6.60
+const tipoDeCombustivel = 'Gasolina';
 const kmPorLitros = 15;
 const distanciaEmKm = 102;
 
 const litrosConsumidos = distanciaEmKm / kmPorLitros;
-const valorGasto = litrosConsumidos * precoCombustivel;
+
+if (tipoDeCombustivel) === 'Etanol' {
+    const valorGasto = litrosConsumidos * precoEtanol;
 console.log(valorGasto.toFixed(2));
+} else {
+const valorGasto = litrosConsumidos * precogasolina;
+console.log(valorGasto.toFixed(2));
+}
