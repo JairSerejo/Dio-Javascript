e038f890dbeb3872ef97a26e7f91371fa870cdc6
JairSerejo <j.serejoqi@gmail.com>
2022-11-08 16:19:47 -0300

    Atualização da aula teste java

--- aulas/variáveis e operadpres/desafio.js
+++ aulas/variáveis e operadpres/desafio.js
@@ -1,5 +1,16 @@
+/* Faça um programa para caucular o gasto com um consumo de combustivel 
+em uma viagem.
 
+Você tera 3 variaveis. sendo elas:
+1 - preço do combustivel;
+2 - gasto medio de combustivel do carro por km;
+3  - distancia em km da viagem;
 
+*/
+const precoCombustivel = 5.79;
+const kmPorLitros = 1500;
+const distanciaEmKm = 102;
 
-
-console.log("ola");
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+const valorGasto = litrosConsumidos * precoCombustivel;
+console.log(valorGasto);
