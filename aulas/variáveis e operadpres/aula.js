b6749716ebb32fe97bfdf58ba0f8cf9b947f675b
JairSerejo <j.serejoqi@gmail.com>
2022-11-08 21:56:21 -0300

    organização

--- aulas/variáveis e operadpres/aula.js
+++ aulas/variáveis e operadpres/aula.js
@@ -1,6 +1,12 @@
 
+const numero = 10;
+const numeroDivisivelPor5 = (numero % 5) === 0;
 
-let variavel = 10 + 10;
-
-console.log(variavel);
-
+if (numero === 0) {
+    console.log("O numero é invalido");
+} else if (numeroDivisivelPor5) {
+     console.log("sim"); 
+} else {
+    console.log("não");
+}
+    
