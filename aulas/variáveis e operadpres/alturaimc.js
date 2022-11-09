1f4399d5133fb0016ae77d3e734d96ae78439052
JairSerejo <j.serejoqi@gmail.com>
2022-11-08 21:24:31 -0300

    treinamento teste 2

---
+++ aulas/variáveis e operadpres/alturaimc.js
@@ -0,0 +1,27 @@
+/*
+Formula do IMC
+IMC = Peso / ( Altura * Altura)
+
+Elabore um algorimo que dado o peso e altura de um adulto mostre a sua
+condição de acordo com a tabela abaixo.
+
+IMC Adulto e condições:
+- Abaixo de 18.5 - abaixo do peso;
+- Entre 18.5 e 25 - peso ideal;
+- Entre 25 à 30 - acima do peso;
+- Entre 30 e 40 - obeso;
+- Acima de 40 - obesidade grave;
+*/
+
+const peso = 78
+const altura = 1.70
+const imc = peso / (altura * altura)
+if (imc <18.5) {
+    console.log ('abaixo do peso');
+} else if (imc >=18.5 && imc <=25) {
+    console.log ('peso ideal');
+} else if (imc >25 && imc <=30) {
+    console.log ('acima do peso');
+} else {
+    console.log ('obesidade grave');
+}
