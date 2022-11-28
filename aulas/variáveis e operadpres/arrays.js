7e27e4b6e6d0f367e5c8d2e476d6a0e09e8bcfe7
JairSerejo <j.serejoqi@gmail.com>
2022-11-27 21:49:33 -0300

    treinamento arrays

--- aulas/variáveis e operadpres/arrays.js
+++ aulas/variáveis e operadpres/arrays.js
@@ -1,6 +1,6 @@
 
 
-const notas = []
+const notas = [];
 
 notas.push (5);
 notas.push (7);
@@ -11,8 +11,8 @@ notas.push (6);
 let soma = 0;
 
 for (let i = 0; i < notas.length; i++) {
-    const notas = notas[i];
-    soma = soma + notas;
+    const nota = notas[i];
+    soma = soma + nota;    
 }
 
 const media = soma / notas.length;
